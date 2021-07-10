2d167b1d4d1898fc7070ef0a177d4331e93fd900
Iron-E <code.iron.e@gmail.com>
2021-07-10 00:37:42 -0400

    perf(callbacks): simplify structure
    We had been using `Callbacks.register` to hold callbacks which users
    provide, by we can just put those directly into `Callbacks`

--- lua/cartographer/callbacks.lua
+++ lua/cartographer/callbacks.lua
@@ -1,17 +1,14 @@
 --- A Neovim plugin to create more straightforward syntax for Lua `:map`ping and `:unmap`ping.
 --- @module callbacks
 --- @alias Callbacks table
-local Callbacks = setmetatable({register = {}},
-{
-	__index = function(self, k) return rawget(self, k) or self.register[k] end
-})
+local Callbacks = {}
 
 --- Register a callback to be
 --- @param cb function the callback
 --- @return number id the handle of the callback
 function Callbacks.new(cb)
-	table.insert(Callbacks.register, cb)
-	return #Callbacks.register
+	Callbacks[#Callbacks+1] = cb
+	return #Callbacks
 end
 
 return Callbacks
