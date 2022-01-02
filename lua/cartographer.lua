2c89288e5cdbb2a00865ffcb8b558da75ddabb50
Iron-E <code.iron.e@gmail.com>
2022-01-02 15:00:12 -0500

    ref!(cartographer): simplify module
    This module is difficult to parse when revisiting. There were many
    `new()` calls, and many tables which seemed to do the same things.
    `MetaCartographer`? `Cartographer`? Why not make it the same?
    
    BREAKING CHANGE: calling the table like in `0.1` is no longer supported.

--- lua/cartographer.lua
+++ lua/cartographer.lua
@@ -12,6 +12,7 @@ local function new() return {_modes = {}} end
 
 --- Make a deep copy of opts table
 --- @param tbl table the table to copy
+--- @return table copy
 local function copy(tbl)
 	local new_tbl = new()
 
@@ -24,47 +25,47 @@ local function copy(tbl)
 	return new_tbl
 end
 
---- The fluent interface `:map`s. Used as a metatable.
-local MetaCartographer
-MetaCartographer =
-{
-	--- Set `key` to `true` if it was not already present
-	--- @param self table the collection of settings
-	--- @param key string the setting to set to `true`
-	--- @returns table self so that this function can be called again
-	__index = function(self, key)
-		self = copy(self)
+--- A fluent interface to create more straightforward syntax for Lua |:map|ping and |:unmap|ping.
+--- @class Cartographer
+--- @field buffer number the buffer to apply the keymap to.
+--- @field _modes table the modes to apply a keymap to.
+local Cartographer = {}
 
-		if #key < 2 then -- set the mode
-			self._modes[#self._modes+1] = key
-		elseif #key > 5 and key:sub(1, 1) == 'b' then -- PERF: 'buffer' is the only 6-letter option starting with 'b'
-			self.buffer = #key > 6 and tonumber(key:sub(7)) or 0 -- NOTE: 0 is the current buffer
-		else -- the fluent interface
-			self[key] = true
-		end
+--- Set `key` to `true` if it was not already present
+--- @param key string the setting to set to `true`
+--- @returns table self so that this function can be called again
+function Cartographer:__index(key)
+	self = copy(self)
 
-		return setmetatable(self, MetaCartographer)
-	end,
+	if #key < 2 then -- set the mode
+		self._modes[#self._modes+1] = key
+	elseif #key > 5 and key:sub(1, 1) == 'b' then -- PERF: 'buffer' is the only 6-letter option starting with 'b'
+		self.buffer = #key > 6 and tonumber(key:sub(7)) or 0 -- NOTE: 0 is the current buffer
+	else -- the fluent interface
+		self[key] = true
+	end
 
-	--- Set a `lhs` combination of keys to some `rhs`
-	--- @param self table the collection of settings and the mode
-	--- @param lhs string the left-hand side |key-notation| which will execute `rhs` after running this function
-	--- @param rhs string if `nil`, |:unmap| lhs. Otherwise, see |:map|.
-	__newindex = function(self, lhs, rhs)
-		local buffer = rawget(self, 'buffer')
-		local modes = rawget(self, '_modes')
-		modes = #modes > 0 and modes or {''}
+	return setmetatable(self, Cartographer)
+end
 
-		if rhs then
-			local opts =
-			{
-				expr = rawget(self, 'expr'),
-				noremap = rawget(self, 'nore'),
-				nowait = rawget(self, 'nowait'),
-				script = rawget(self, 'script'),
-				silent = rawget(self, 'silent'),
-				unique = rawget(self, 'unique'),
-			}
+--- Set a `lhs` combination of keys to some `rhs`
+--- @param lhs string the left-hand side |key-notation| which will execute `rhs` after running this function
+--- @param rhs string|nil if `nil`, |:unmap| lhs. Otherwise, see |:map|.
+function Cartographer:__newindex(lhs, rhs)
+	local buffer = rawget(self, 'buffer')
+	local modes = rawget(self, '_modes')
+	modes = #modes > 0 and modes or {''}
+
+	if rhs then
+		local opts =
+		{
+			expr = rawget(self, 'expr'),
+			noremap = rawget(self, 'nore'),
+			nowait = rawget(self, 'nowait'),
+			script = rawget(self, 'script'),
+			silent = rawget(self, 'silent'),
+			unique = rawget(self, 'unique'),
+		}
 
 		if type(rhs) == 'function' then
 			if Callbacks then -- TODO: remove when `0.7` is stabilized
@@ -79,36 +80,26 @@ MetaCartographer =
 			opts.noremap = true
 		end
 
-			if buffer then
-				for _, mode in ipairs(modes) do
-					vim.api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
-				end
-			else
-				for _, mode in ipairs(modes) do
-					vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
-				end
+		if buffer then
+			for _, mode in ipairs(modes) do
+				vim.api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
 			end
 		else
-			if buffer then
-				for _, mode in ipairs(modes) do
-					vim.api.nvim_buf_del_keymap(buffer, mode, lhs)
-				end
-			else
-				for _, mode in ipairs(modes) do
-					vim.api.nvim_del_keymap(mode, lhs)
-				end
+			for _, mode in ipairs(modes) do
+				vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 			end
 		end
-	end,
-}
+	else
+		if buffer then
+			for _, mode in ipairs(modes) do
+				vim.api.nvim_buf_del_keymap(buffer, mode, lhs)
+			end
+		else
+			for _, mode in ipairs(modes) do
+				vim.api.nvim_del_keymap(mode, lhs)
+			end
+		end
+	end
+end
 
---- A Neovim plugin to create more straightforward syntax for Lua `:map`ping and `:unmap`ping.
---- @module nvim-cartographer
---- @return table Cartographer a fluent interface for `:map` / `:unmap` interaction
-return setmetatable(new(),
-{
-	-- NOTE: For backwards compatability. `__index` is preferred.
-	__call = function(_) return setmetatable(new(), MetaCartographer) end,
-	__index = function(_, key) return setmetatable(new(), MetaCartographer)[key] end,
-	__newindex = MetaCartographer.__newindex,
-})
+return setmetatable(new(), Cartographer)
